66693aa889aeeadba700aeaa42a9099f96fe47fc
IsmailsGit <IsmailMahdale12@gmail.com>
2025-09-09 17:09:48 +0100

    Was changing the host for the app.py

--- hello_flask_redis_challenge1/app.py
+++ hello_flask_redis_challenge1/app.py
@@ -4,7 +4,7 @@ import redis
 app = Flask(__name__)
 
 r = redis.Redis( #Instantiate a Redis client, connecting to local host on port 6379
-     host="localhost",
+     host="host.docker.internal",
      port=6379,
      db=0 #The default Redis database index
 )
@@ -22,25 +22,13 @@ def count_page():
    #  r.get("visit_count")
    #  return f"This is the value: {value.decode()}"
      count = r.get("visit_count")
-     return f"This is the visit count: {visiters.decode()}"
+     return f"This is the visit count: {count.decode()}"
 if __name__ == "__main__": 
     app.run(host="0.0.0.0", port=5000)
 
+#To make this work run redis first by doing docker run -d --name my-redis -p 6379:6379 redis:latest
+#Then you can build the image docker build -t redis-flask .
+#And finally to run the container you do docker run -p 5000:5000 redis-flask
+#If you make a docker-compose.yml file there is no need to do any of this ^
 
 
-
-
-
-
-#from flask import Flask  
-
-#app = Flask(__name__) 
-
-#@app.route("/") 
-#def hello(): 
-#     return "Hello, Flask!" 
-#@app.route("/greet") 
-#def greet(): 
- #    return "Hello from the /greet route!" 
-#if __name__ == "__main__": 
- #    app.run(host="0.0.0.0", port=5000)
