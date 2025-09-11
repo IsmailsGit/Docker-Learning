b3464698643bc2661bc5f4c8d79421f26d700293
IsmailsGit <IsmailMahdale12@gmail.com>
2025-09-11 09:26:09 +0100

    Made me flask-redis app and visit counter work

--- hello_flask_redis_challenge1/app.py
+++ hello_flask_redis_challenge1/app.py
@@ -4,7 +4,7 @@ import redis
 app = Flask(__name__)
 
 r = redis.Redis( #Instantiate a Redis client, connecting to local host on port 6379
-     host="host.docker.internal",
+     host="redis",
      port=6379,
      db=0 #The default Redis database index
 )
@@ -15,14 +15,11 @@ def home_page():
 
 @app.route("/count")
 def count_page():
-#SET command: store a string value under 'foo'
-  #   r.set("visit_count")
+
      r.incr("visit_count")
-#GET command: retrieve the value stored at 'foo'
-   #  r.get("visit_count")
-   #  return f"This is the value: {value.decode()}"
      count = r.get("visit_count")
      return f"This is the visit count: {count.decode()}"
+
 if __name__ == "__main__": 
     app.run(host="0.0.0.0", port=5000)
 
